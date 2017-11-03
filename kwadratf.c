7dc21e092278b2c6533f91527936320e7b8ed9f9
JacekK87 <jacekielar1@wp.pl>
2017-11-03 11:35:12 +0100

    sqrt

--- kwadratf.c
+++ kwadratf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
  float  a, b , c; 
- float delta, x0, x1, x2, x;
+ float delta, x0, x1, x2;
  float main()
 {	
 
@@ -11,29 +11,41 @@
 		if(a==0)
 		{
 				printf("Wartosc a nie moze byc rowna 0\n");
-			}else
-			
+			}
+		else
+		{	
 
 		printf("Podaj wartosc argumentu b:  "); scanf("%f"  ,&b);
 		printf("Podaj wartosc argumentu c:  "); scanf("%f"  ,&c);
 			
 			delta=0;
 			delta=(b*b)-(4*a*c);
-			printf("Delta wynosi: %f\n", delta);
+			printf("Delta wynosi: %f\n",delta);
+	if(delta>0) 
+                {	delta=sqrt(delta);
+                        x1=(-b-(delta))/(2*a);
+                        x2=(-b+(delta))/(2*a);
+                printf( "Dwa miejsca zerowe: x1= %f i x2=%f \n ",x1 ,x2);
+                }
+	else 
+		{
 
-	if(delta==0) {
-			x0=-b/(2*a);
-		printf(" Jeden punkt zerowy:x0= %f\n", x0);}
-		else if(delta>0) {
-			x1=(-b-delta)/(2*a);
-			x2=(-b+delta)/(2*a);
- printf( "Dwa miejsca zerowe: x1= %f i x2=%f \n ",x1 ,x2);}
-		else if (delta<0) { printf("Brak miejsc zerowych \n");}
-		else {
+	if(delta==0)
+		 {
+			x0=-b/2*a;
+			printf(" Jeden punkt zerowy:x0= %f\n", x0);
+		}
+	else  
+		{
+	 if (delta<0) 
+		{
+			 printf("Brak miejsc zerowych \n");
+			return 0;
+			}
+		}
 	}
 
-
- return ( x1 ,x2, x0);
-
+ return 0;
+}
  }
 
